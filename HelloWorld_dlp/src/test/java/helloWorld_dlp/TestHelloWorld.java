94951205c01ff6818f1c94b54da6e495f14ebfb3
kumar <alwaysdilip1989@gmail.com>
2021-06-30 12:42:38 +0530

    First Commit

---
+++ HelloWorld_dlp/src/test/java/helloWorld_dlp/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package helloWorld_dlp;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		System.out.println("Hello world... ");
+	}
+
+}
